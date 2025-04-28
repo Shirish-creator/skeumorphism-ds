0567e828d37c63a6f5d5f2b499a4accd2d075f35
Shirish Shakya <84584836+Shirish-creator@users.noreply.github.com>
2025-04-28 18:07:00 -0400

    bred btn

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -37,7 +37,7 @@ export const Button = ({
     display: 'inline-block',
     fontWeight: 700,
     lineHeight: 1,
-    backgroundColor: backgroundColor || (primary ? '#FDF5F5' : '#ffffff'),
+    backgroundColor: backgroundColor || (primary ? '#FF0E0E' : '#ffffff'),
     color: primary ? '#000' : '#333',
     boxShadow: primary
       ? '0 0 0 1px #1ea7fd'
