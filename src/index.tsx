00a6033d932498cf4d620d99195379a0a6a0e476
Shirish Shakya <84584836+Shirish-creator@users.noreply.github.com>
2025-05-03 20:26:53 -0400

    button directory changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export {ThemeProvider} from "./context/ThemeContext";
 export {useTheme} from "./context/ThemeContext";
 export { lightTheme, darkTheme } from './context/ThemeContext';
-export {Button} from './components/Button';
+export {Button} from './components/Button/Button';
