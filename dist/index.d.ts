959fa2e15d4db8beb3df6c1c6f357d515e462624
Shirish Shakya <84584836+Shirish-creator@users.noreply.github.com>
2025-05-03 20:31:05 -0400

    new build added

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export { ThemeProvider } from "./context/ThemeContext";
 export { useTheme } from "./context/ThemeContext";
 export { lightTheme, darkTheme } from './context/ThemeContext';
-export { Button } from './components/Button';
+export { Button } from './components/Button/Button';
 //# sourceMappingURL=index.d.ts.map
